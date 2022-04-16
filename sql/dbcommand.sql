4a726db9338c1bb66919b7cf391b3bee4cd9b9e4
kingmufasa99 <mmfil2@ulaval.ca>
2022-04-15 20:59:32 -0400

    hh

--- sql/dbcommand.sql
+++ sql/dbcommand.sql
@@ -1,13 +1,22 @@
 CREATE DATABASE rentmyappartment;
 USE rentmyappartment;
 CREATE TABLE password(PID char(9) primary key, password text);
-CREATE TABLE user(UID char(12) NOT NULL, name char(100), email char(100), PID char(12), PRIMARY KEY(UID), FOREIGN KEY(PID) REFERENCES password(PID));
-CREATE TABLE appartments(AID char(12) NOT NULL, title char(100), address char(100), appartment char(5), zipCode char(100),town char(100),size char(10),price int, description text,coordinate text,UID char(12) not null,PRIMARY KEY(AID),FOREIGN KEY(UID) REFERENCES user(UID));
+CREATE TABLE user(UID char(12) NOT NULL, name char(100), email char(100), PID char(12), PRIMARY KEY(UID), 
+                    FOREIGN KEY(PID) REFERENCES password(PID));
+
+
+CREATE TABLE GrandeurAppt (GID char(12) primary key NOT NULL, size float(2) );
 CREATE TABLE City(CID char(12), 
                 CityName VARCHAR(64), 
                 Country VARCHAR(64), 
                 CoordinateCity text,
                 PRIMARY KEY(CID));
 
-CREATE TABLE GrandeurAppt (AID char(12) foreign key NOT NULL, size float(2) primary key );
+CREATE TABLE appartments( AID char(12) NOT NULL, title char(100), address char(100), numeroAppt char(5), 
+            zipCode char(100), price int, description text, UID char(12) not null,PRIMARY KEY(AID), 
+            CID char(12) not null, GID char(12) not null,
+            FOREIGN KEY(UID) REFERENCES user(UID), foreign key (CID) REFERENCES city (CID),
+            foreign key (GID) REFERENCES GrandeurAppt (GID));
+
+
 
