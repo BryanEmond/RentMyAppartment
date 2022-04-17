4c1e195637244e2142a50325106b19404964b103
kingmufasa99 <mmfil2@ulaval.ca>
2022-04-17 14:24:49 -0400

    add appt

--- sql/dbcommand.sql
+++ sql/dbcommand.sql
@@ -3,14 +3,16 @@ USE rentmyappartment;
 
 
 CREATE TABLE password(PID char(9) primary key, password text);
-CREATE TABLE user(UID char(100) NOT NULL, first_name char(100), last_name char(100), middle_name char(100), PID char(12), PRIMARY KEY(UID), 
+CREATE TABLE user(UID char(100) NOT NULL, PRIMARY KEY(UID), first_name char(100), 
+                    last_name char(100), middle_name char(100), PID char(12),  
                     FOREIGN KEY(PID) REFERENCES password(PID));
 CREATE TABLE GrandeurAppt (GID char(20) primary key NOT NULL);
 CREATE TABLE Country(country_name char(12));
 
-CREATE TABLE City(CID char(100), 
-                region char (100), arrondissement char (100), nombre_appt int,
-                country_name char(12), PRIMARY KEY(CID), foreign key (country_name) REFERENCES Country(country_name);
+CREATE TABLE City(CID char(100), region char (100), 
+                arrondissement char (100), nombre_appt int,
+                country_name char(12), 
+                PRIMARY KEY(CID), foreign key (country_name) REFERENCES Country(country_name);
 
 CREATE TABLE appartments( AID char(100) NOT NULL, PRIMARY KEY(AID), 
             price int, description text, UID char(12) not null, 
@@ -22,7 +24,7 @@ CREATE TABLE appartments( AID char(100) NOT NULL, PRIMARY KEY(AID),
 INSERT INTO password('001',' naruto');
 INSERT INTO password('002', 'sasuke');
 INSERT INTO user ('mfilali99@hotmail.com', 'Moulay', 'Mostafa', 'Filali', '001');
-INSERT INTO user('bryanemond@hotmail.com', 'Bryan', ' ', 'Emond', '002');
+INSERT INTO user ('bryanemond@hotmail.com', 'Bryan', ' ', 'Emond', '002');
 INSERT INTO GrandeurAppt ('un-et-demi');
 INSERT INTO GrandeurAppt ('deux-et-demi');
 INSERT INTO GrandeurAppt ('trois-et-demi');
@@ -31,17 +33,19 @@ INSERT INTO GrandeurAppt ('cinq-et-demi');
 INSERT INTO Country('Canada');
 INSERT INTO Country('USA');
 
-INSERT INTO City('Quebec', 'Les Saules', 'Lebourgneuf', 45,  'Canada');
-INSERT INTO City('Trois-Rivières', 'Canada');
-INSERT INTO City('Montréal', 'Canada');
-INSERT INTO City('New York', 'USA');
+INSERT INTO City('Quebec', 'Les Saules', 'Lebourgneuf', 45, 'Canada');
+INSERT INTO City('Quebec', 'Charlesbourg', 'Limoilou', 45, 'Canada');
+INSERT INTO City('Montréal', 'Montréal-Nord', 'Cote-des-Neiges','Canada');
 
 INSERT INTO appartments VALUES (
             '6270 rue dAlesia appt. 50', 520, 'Studio meublé', 
-            'mfilali99@hotmail.com', 'Quebec', 'Canada', 'un-et-demi', FALSE),
+            'mfilali99@hotmail.com', 'Quebec', 'Canada', 'un-et-demi', FALSE
+            )
+            ,
             (
             '6268 rue dAlesia appt. 48', 800, 'Appartement meublé 2 chambres', 
-            'mfilali99@hotmail.com', 'Quebec', 'Canada', '4-et-demi', FALSE)
+            'mfilali99@hotmail.com', 'Quebec', 'Canada', '4-et-demi', FALSE
+            )
 
 
 DROP TRIGGER IF EXISTS t_appt_delete;
