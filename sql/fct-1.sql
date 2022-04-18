b9744d8a8bb46ded0a09dd3f6511f9ce5d3b79ca
kingmufasa99 <mmfil2@ulaval.ca>
2022-04-18 16:15:27 -0400

    m

--- sql/fct-1.sql
+++ sql/fct-1.sql
@@ -6,8 +6,20 @@
     lastName char(100)NOT NULL,
     middleName char(100),
     PASSWORD1 char(100)NOT NULL
-    ) 
+    )
 begin
     insert into password values (EMAIL, PASSWORD1)
 end //
 delimiter ;
+
+delimiter //
+
+create FUNCTION selection_appt_grandeur RETURNS VARCHAR
+begin
+
+SELECT * INTO size FROM GrandeurAppt;
+    RETURN size;
+
+END //
+delimiter ;
+
